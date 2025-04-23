e049a5415937c26fb6dc9c423f66bf2a37066cc2
Manh <dtmanh1703@gmail.com>
2025-04-23 17:16:34 +0700

    update DMEM IMEM module

--- rtl/DMEM.v
+++ rtl/DMEM.v
@@ -4,24 +4,30 @@
 //-----------------------------------------------------------------
 // Data_Memory
 
-module Data_Memory(clk, rst_n, MemRW, read_address, Write_data, MemData_out);
+module Data_Memory(clk, rst_n, MemRW, addr, DataW, DataR);
     input clk, rst_n, MemRW;
-    input [31:0] read_address, Write_data;
-    output [31:0] MemData_out;
+    input [31:0] addr, DataW;
+    output [31:0] DataR;
+
     integer k;
     reg [31:0] D_Mem [63:0];
+
+    always @(posedge clk) begin
+	D_Mem[17] <= 56;
+	D_Mem[15] <= 65;
+    end
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             for (k = 0; k < 64; k = k + 1) begin
-                D_Mem[k] = 32'b00;
+                D_Mem[k] <= 32'b00;
             end
         end
-        else if (!MemRW) begin
-            D_Mem[read_address] = Write_data;
+        else if (!MemRW) begin //MemRW =0 => store the DataW 
+            D_Mem[addr] <= DataW;
         end
     end
 
-    assign MemData_out = (MemRW) ? D_Mem[read_address] : 32'b0;
+    assign DataW = ( MemRW ) ? D_Mem[addr] : 32'b0; //MemRW =1 => use DMEM , MEMRW=0 not using DMEM
 
 endmodule
