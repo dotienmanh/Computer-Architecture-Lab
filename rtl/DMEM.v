3eb8686eb27e629c405786d6f8d57cea35657b1c
Manh <dtmanh1703@gmail.com>
2025-04-24 01:46:00 +0700

    update IMEM DMEM

--- rtl/DMEM.v
+++ rtl/DMEM.v
@@ -1,33 +1,45 @@
-//-----------------------------------------------------------------
-//                      RISC-V Core Single Cycle Processor
-//                                CA Lab
-//-----------------------------------------------------------------
-// Data_Memory
+module Data_Memory(
+ input clk,
+ // address input, shared by read and write port
+ input [31:0]   addr,
+ 
+ // write port
+ input [31:0]   DataW,
+ input     MemRW,
+ // read port
+ output [31:0]   DataR
+);
 
-module Data_Memory(clk, rst_n, MemRW, addr, DataW, DataR);
-    input clk, rst_n, MemRW;
-    input [31:0] addr, DataW;
-    output [31:0] DataR;
+reg [31:0] Memory [255:0];
+integer f;
+wire [2:0] ram_addr=addr[2:0];
+initial
+ begin
+  $readmemb("./test.data", Memory);
+  
+  // Open file for logging memory content
+  f = $fopen("./test.o", "w");
+  
+  // Log memory contents (first time)
+  $fdisplay(f, "time = %d\n", $time);
+  $fdisplay(f, "\tmemory[0] = %b", Memory[0]);
+  $fdisplay(f, "\tmemory[1] = %b", Memory[1]);
+  $fdisplay(f, "\tmemory[2] = %b", Memory[2]);
+  $fdisplay(f, "\tmemory[3] = %b", Memory[3]);
+  $fdisplay(f, "\tmemory[4] = %b", Memory[4]);
+  $fdisplay(f, "\tmemory[5] = %b", Memory[5]);
+  $fdisplay(f, "\tmemory[6] = %b", Memory[6]);
+  $fdisplay(f, "\tmemory[7] = %b", Memory[7]);
 
-    integer k;
-    reg [31:0] D_Mem [63:0];
-
-    always @(posedge clk) begin
-	D_Mem[17] <= 56;
-	D_Mem[15] <= 65;
-    end
-
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            for (k = 0; k < 64; k = k + 1) begin
-                D_Mem[k] <= 32'b00;
-            end
-        end
-        else if (!MemRW) begin //MemRW =0 => store the DataW 
-            D_Mem[addr] <= DataW;
-        end
-    end
-
-    assign DataW = ( MemRW ) ? D_Mem[addr] : 32'b0; //MemRW =1 => use DMEM , MEMRW=0 not using DMEM
+  #320; // Wait for 320 time units before closing the file
+  $fclose(f);
+ 
+ end
+ 
+ always @(posedge clk) begin
+  if (!MemRW)   //When MemRW =0 , we want to write data
+   Memory[ram_addr] <= DataW;
+ end
+ assign DataR = (MemRW ==1'b1) ? Memory[ram_addr]: 32'd0; 
 
 endmodule
